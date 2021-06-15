886c009f22c1a1b522b413299cab8bfafb8a5f4d
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-06-15 15:07:38 +0200

    creacion de componentes y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ListComponent } from './components/list/list.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListComponent,
+    RegisterComponent,
+    HeaderComponent,
+    HomeComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
